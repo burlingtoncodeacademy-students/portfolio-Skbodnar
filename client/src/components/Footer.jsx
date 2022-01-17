fd4e9f97952c36220fcbd7c4a3414f01eef09253
Sophie <sophiekbodnar@gmail.com>
2022-01-17 13:20:33 -0500

    fixed links to github and linkedIn in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 import git from "../images/git.png";
 import linkedIn_icon from "../images/linkedIn_icon.png";
 import "../styles/main.css";
@@ -7,13 +8,15 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        <img src="https://github.com/Skbodnar" id="icon" src={git} />
-
-        <img
-          src="https://www.linkedin.com/in/sophie-bodnar-8b7ba51a4/"
-          id="icon"
-          src={linkedIn_icon}
-        />
+        <a href="https://github.com/Skbodnar" >
+          <img src={git} id="icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sophie-bodnar-8b7ba51a4/"
+         
+        >
+          <img src={linkedIn_icon}  id="icon" />
+        </a>
       </div>
     </>
   );
